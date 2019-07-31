36e6a6fcfd05ad7f407fa4bf14ceca61508bfa5f
garbirel94 <52559489+garbirel94@users.noreply.github.com>
2019-07-31 11:23:38 +0800

    Update header.dart

--- lib/src/header/header.dart
+++ lib/src/header/header.dart
@@ -259,7 +259,7 @@ class ClassicsHeader extends RefreshHeader {
       this.refreshReadyText: "Segarkan",
       this.refreshingText: "Segarkan...",
       this.refreshedText: "Segarkan selesai",
-      this.bgColor: Color(0xffE4E4E4),
+      this.bgColor: Colors.deepOrange,
       this.textColor: Colors.white,
       this.moreInfoColor: Colors.white,
       this.refreshHeight: 70.0,
